43e73cfab9867d3c7592d614c32eeb3b944f82c7
Claudia Manchon <claudiambomb@gmail.com>
2022-03-15 16:15:36 +0800

    add-features
    added score feature
    added stop/pause button
    added restart button
    new style

--- script.js
+++ script.js
@@ -1,9 +1,100 @@
 const player = document.getElementById("player");
+const cactus = document.getElementById("cactus");
+const background = document.getElementById("background");
+const restartButton = document.getElementById("restartGame");
+const board = document.getElementById("board");
 
-document.addEventListener("click", function () {
-  player.classList.add("playerJump")
+let scoreInterval;
+let score = 0;
+
+const buttonPlayStop = document.getElementById("buttonPlayerStop")
+
+board.addEventListener("click", function () {
+  playerJump();
 });
 
 document.addEventListener("animationend", function () {
-  player.classList.remove("playerJump")
+  removeJump();
 });
+
+function playerJump() {
+  player.classList.add("playerJump")
+
+}
+
+function removeJump() {
+  player.classList.remove("playerJump")
+
+}
+
+
+function pauseGame() {
+  stopAnimations();
+  stopInterval();
+}
+
+function stopAnimations() {
+  cactus.style.animationPlayState = 'paused';
+  player.style.animationPlayState = 'paused';
+  background.style.animationPlayState = 'paused';
+}
+
+function stopInterval() {
+  clearInterval(scoreInterval);
+}
+
+function resumeGame() {
+  playAnimations();
+}
+
+function playAnimations() {
+  cactus.style.animationPlayState = 'running';
+  player.style.animationPlayState = 'running';
+  background.style.animationPlayState = 'running';
+}
+
+buttonPlayStop.addEventListener("click", () => {
+  buttonPlayStop.classList.toggle("play");
+
+  if (buttonPlayStop.classList.contains("play")) {
+    // si es PLAY -- tiene la clase play -- continua el juego
+    resumeGame();
+    resumeInterval();
+
+  } else {
+    // si es PAUSE --  no tiene la clase play -- paramos las animaciones
+    pauseGame();
+  };
+})
+
+
+
+function resumeInterval() {
+  scoreInterval = setInterval(() => {
+    score++;
+    document.getElementById("score").innerText = score;
+  }, 1000);
+}
+
+resumeInterval();
+
+restartButton.addEventListener("click", restartGame);
+
+function restartGame() {
+  resetScore();
+  removeJump();
+  cactus.classList.remove("cactusMovement");
+  void cactus.offsetWidth;
+  cactus.classList.add("cactusMovement");
+
+}
+
+function resetScore() {
+  score = 0;
+}
+
+document.addEventListener("keyup", (event) => {
+  if (event.key == " " || event.key == "ArrowUp"){
+    playerJump();
+  }
+})
